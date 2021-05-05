87ccf8534717afd8dca1344cb649a635c132c741
Konstantin Stepanov <me@kstep.me>
2021-05-05 14:42:31 +0300

    fix format

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,6 @@ pub mod service;
 pub mod util;
 pub mod validator;
 
-pub use validator::TestValidatorService;
-pub use service::TestServiceProcess;
 pub use program::{Network, Program};
+pub use service::TestServiceProcess;
+pub use validator::TestValidatorService;
